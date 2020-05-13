3955ebe401850ebc38a2e73856c204d7888b4c00
Adminixtrator <sadiqheritage927@gmail.com>
2020-05-13 09:14:00 +0100

    Reincarnation

--- Test2.py
+++
@@ -1,7 +0,0 @@
-from os import listdir
-from numpy import asarray
-from numpy import save
-from keras.preprocessing.image import load_img
-from keras.preprocessing.image import img_to_array
-
-folder = 'test/'
